bac0a31bd4bac8618bb2daaa064575300838ec8f
Nathan Rothenberger <nroth10@gmail.com>
2022-01-08 08:12:17 -0600

    adjust single post page stuff

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -6,7 +6,8 @@ type Props = {
 
 const PostBody = ({ content }: Props) => {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-3xl mr-auto">
+      post body
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
