2801134c64b4fea52c67cc7b68cb8c70b16b12a3
Nathan Rothenberger <nroth10@gmail.com>
2022-02-23 19:15:01 -0600

    add project type

---
+++ types/project.ts
@@ -0,0 +1,15 @@
+export type Project = {
+  slug: string;
+  title: string;
+  date: string;
+  // coverImage: string;
+  // author: Author;
+  // excerpt: string;
+  // ogImage: {
+  //   url: string;
+  // };
+  content: string;
+  assetDir: string;
+};
+
+// export default PostType;
