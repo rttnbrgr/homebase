a15f0958519247c846e662559683b5752a64e771
Nathan Rothenberger <nroth10@gmail.com>
2022-08-22 19:56:05 -0500

    refactor more stories component

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,24 +1,42 @@
-import MoreStories from "../../components/more-stories";
 import { Layout } from "../../components/Layout";
 import { getAllPosts } from "../../lib/api";
 import Post from "../../types/post";
 import MetaPage from "../../components/MetaPage";
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, SimpleGrid, Text } from "@chakra-ui/react";
+import { PostPreview } from "../../components/post";
 
 type Props = {
   allPosts: Post[];
 };
 
 const Blog = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout title="Blog">
         <MetaPage title="Blog" />
         <Container variant="refactor">
           <Box textStyle="resetRefactor">
-            {allPosts.length > 0 && <MoreStories posts={allPosts} />}
+            {allPosts.length > 0 && (
+              <Box mb="32" maxW="2xl" mx="auto">
+                <Text as="h2" textStyle={{ base: "h2", md: "h1", lg: "jumbo" }}>
+                  Posts
+                </Text>
+
+                <SimpleGrid columns={1} spacingY="12" pt="4">
+                  {allPosts.map(post => (
+                    <PostPreview
+                      key={post.slug}
+                      title={post.title}
+                      coverImage={post.coverImage}
+                      date={post.date}
+                      author={post.author}
+                      slug={post.slug}
+                      excerpt={post.excerpt}
+                    />
+                  ))}
+                </SimpleGrid>
+              </Box>
+            )}
           </Box>
         </Container>
       </Layout>
