58a4baa258ec0864e4a0d3c4c0e17d785bc21be8
Nathan Rothenberger <nroth10@gmail.com>
2022-09-03 19:32:17 -0500

    wire up nav menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,100 +1,126 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import {
   Box,
   Text,
   Flex,
-  useColorMode,
-  Button,
   Container as ChakraContainer,
-  HStack,
-  IconButton
+  useDisclosure,
+  VStack,
+  BoxProps
 } from "@chakra-ui/react";
 import { miscLinks } from "../lib/homepageLinks";
 import { MonoLinkFork } from "./MonoLink";
 import { Breadcrumb, BreadcrumbProps } from "./Breadcrumb";
-import { CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
+import { ColorModetoggle } from "./ColorModeToggle";
+import { NavMenuToggle } from "./NavMenuToggle";
 
-type ColorModetoggleProps = {};
+/**
+ * Nav Menu
+ */
 
-const ColorModetoggle = ({}: ColorModetoggleProps) => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const isLightMode = useMemo(() => colorMode === "light", [colorMode]);
-  const icon = isLightMode ? <SunIcon /> : <MoonIcon />;
-  // const icon = isLightMode ? <HamburgerIcon /> : <CloseIcon/>
+const offset = "72px";
+
+const MenuDrawerStyles: BoxProps = {
+  position: "absolute",
+  zIndex: 1,
+  // Add watcher to manage this
+  top: offset,
+  right: 0,
+  left: 0,
+  // px: 4,
+  px: "1.5rem",
+  pr: 24,
+  pt: 2,
+  pb: 8,
+  layerStyle: "debug",
+  transition: "transform .3s ease"
+};
+
+function getMenuTransform(isOpen: boolean) {
+  // const offset = "72px";
+  return {
+    transform: `translateY(${isOpen ? "0" : `calc((100% + ${offset}) * -1)`})`
+  };
+}
+
+type NavMenuProps = {
+  isOpen: boolean;
+};
+
+const NavMenu = ({ isOpen }: NavMenuProps) => {
+  const menuTransform = useMemo(() => getMenuTransform(isOpen), [isOpen]);
 
   return (
-    <>
-      <IconButton
-        variant="base"
-        aria-label="Toggle color mode"
-        icon={icon}
-        onClick={toggleColorMode}
-      />
-    </>
+    <Box {...MenuDrawerStyles} sx={menuTransform}>
+      <VStack spacing="4" align="start">
+        {miscLinks
+          .filter(x => x.showInNav)
+          .map(({ linkText, ...props }, j) => {
+            return (
+              <Text textStyle="homeRoot" lineHeight="1" key={j}>
+                <MonoLinkFork key={j} {...props}>
+                  {linkText}
+                </MonoLinkFork>
+              </Text>
+            );
+          })}
+      </VStack>
+    </Box>
   );
 };
 
-type NavMenuToggleProps = {};
+/**
+ * Header
+ */
 
-const NavMenuToggle = ({}: NavMenuToggleProps) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const icon = menuOpen ? <CloseIcon /> : <HamburgerIcon />;
-
-  function toggleMenu() {
-    setMenuOpen(!menuOpen);
-  }
-
-  return (
-    <>
-      <IconButton
-        variant="base"
-        aria-label="Toggle color mode"
-        icon={icon}
-        onClick={toggleMenu}
-      />
-    </>
-  );
+const headerStyles: BoxProps = {
+  position: "relative",
+  zIndex: 1
 };
 
 type HeaderProps = BreadcrumbProps & {};
 
 const Header = ({ title, isHome = false }: HeaderProps) => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  // Menus
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const menuIcon = isOpen ? <CloseIcon /> : <HamburgerIcon />;
+  const menuHandler = isOpen ? onClose : onOpen;
 
   return (
     <>
-      <Box as="header" layerStyle="">
+      {/* Nav Menu */}
+      <NavMenu isOpen={isOpen} />
+
+      {/* Header */}
+      <Box as="header" layerStyle="debug" {...headerStyles}>
         <ChakraContainer variant="refactor" py="4">
           <Flex justifyContent="space-between" alignItems="center">
             {/* Breadcrumb */}
             <Breadcrumb title={title} isHome={isHome} />
 
             {/* Links */}
-            <HStack spacing="6" align="start">
+            <HStack spacing="6" align="center">
               <HStack
                 spacing="4"
                 align="start"
                 display={{ base: "none", sm: "flex" }}>
-                {miscLinks.map(({ linkText, ...props }, j) => {
-                  // Selectively hide nav
-                  if (linkText === "Fullstack Design") {
-                    return null;
-                  }
-                  return (
-                    <Text textStyle="h5" lineHeight="1" key={j}>
-                      <MonoLinkFork key={j} {...props}>
-                        {linkText}
-                      </MonoLinkFork>
-                    </Text>
-                  );
-                })}
+                {miscLinks
+                  .filter(x => x.showInNav)
+                  .map(({ linkText, ...props }, j) => {
+                    return (
+                      <Text textStyle="h5" lineHeight="1" key={j}>
+                        <MonoLinkFork key={j} {...props}>
+                          {linkText}
+                        </MonoLinkFork>
+                      </Text>
+                    );
+                  })}
               </HStack>
+              {/* Icon Stack */}
               <HStack spacing="0">
-                {/* <Button onClick={toggleColorMode} textStyle="h1">
-                  {colorMode}
-                </Button> */}
                 <ColorModetoggle />
-                <NavMenuToggle />
+                <NavMenuToggle icon={menuIcon} onClick={menuHandler} />
               </HStack>
             </HStack>
           </Flex>
